dc8e406bfa4ba958176356548078f9d666e8b10f
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-18 11:28:57 +0530

    Add files via upload

---
+++ UC5/src/main/java/com/BridgeLabz/AddressBook/App/DTO/AddressBookDTO.java
@@ -0,0 +1,9 @@
+package com.BridgeLabz.AddressBook.App.DTO;
+import lombok.Data;
+
+@Data
+public class AddressBookDTO {
+    private String name;
+    private String phone;
+    private String address;
+}
