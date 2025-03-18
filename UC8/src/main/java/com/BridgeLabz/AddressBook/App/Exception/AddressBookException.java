07838954987d55153aa3b2aa2259c0c0917cda8a
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-18 11:43:14 +0530

    Delete UC8 directory

--- UC8/src/main/java/com/BridgeLabz/AddressBook/App/Exception/AddressBookException.java
+++
@@ -1,7 +0,0 @@
-package com.BridgeLabz.AddressBook.App.Exception;
-
-public class AddressBookException extends RuntimeException {
-    public AddressBookException(String message) {
-        super(message);
-    }
-}
