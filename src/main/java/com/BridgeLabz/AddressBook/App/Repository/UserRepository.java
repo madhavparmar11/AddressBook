42ed3e571104c231accecbe3e2a5c9c22a7d09c0
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-18 11:50:11 +0530

    Add files via upload

---
+++ src/main/java/com/BridgeLabz/AddressBook/App/Repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.BridgeLabz.AddressBook.App.Repository;
+
+import com.BridgeLabz.AddressBook.App.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
