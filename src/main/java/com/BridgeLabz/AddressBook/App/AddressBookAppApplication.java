195bee7f01046d2fa9af77e006906f72a5c85e41
Madhav Parmar <152685058+madhavparmar11@users.noreply.github.com>
2025-03-18 11:47:16 +0530

    Add files via upload

---
+++ src/main/java/com/BridgeLabz/AddressBook/App/AddressBookAppApplication.java
@@ -0,0 +1,15 @@
+package com.BridgeLabz.AddressBook.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+public class AddressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressBookAppApplication.class, args);
+	}
+
+}
